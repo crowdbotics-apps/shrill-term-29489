dfbdf943beb59e38f6e5bfed857baba5174d6b8e
Crowdbotics <team@crowdbotics.com>
2021-08-05 01:42:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shrill_term_29489Identifier",
+  url: "https://shrill_term_29489Identifier.botics.co",
+  api: "https://shrill_term_29489Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
